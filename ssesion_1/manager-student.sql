4036107b742849adc125a61f0f48641fd695373b
tinhnguyen269 <tinhnguyen180507@gmail.com>
2024-06-18 14:31:02 +0700

    fisrt commit

---
+++ ssesion_1/manager-student.sql
@@ -0,0 +1,14 @@
+create database student_management;
+use student_management;
+
+create table Class(
+	id int,
+    name varchar(200)
+);
+
+create table Teacher(
+	id int,
+    name varchar(200),
+    age int,
+    contry varchar(50)
+);
